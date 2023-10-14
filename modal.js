27731df51a7be9741edac7b7f7abeb2a78ad955d
roini <r.giorgadze1996@gmail.com>
2023-10-14 21:51:36 +0400

    Update modal.js
    ss

--- modal.js
+++ modal.js
@@ -3,9 +3,23 @@
         var modal = document.getElementById("myModal");
     // როდესაც დავაწვებით span ელემენტს დაიხურება მოდალური გვერდი
         var span = document.getElementsByClassName("close")[0];
-    // როდესაც წებგვერდის ტანი ჩაიტვირთება ამოვარდება მოდლური გვერდი 
+        //როცა ეკრანის რეზოლუცი მეტი იქნება 768-ე მოდალი არ გაიხსნას
+        let width=window.screen.availWidth;
+        if(width<768){
+            // როდესაც წებგვერდის ტანი ჩაიტვირთება ამოვარდება მოდლური გვერდი 
         function load() { modal.style.display = "block";}
+        }
+        else{
+            modal.style.display = "none";
+        }
     // When the user clicks on <span> (x), close the modal
         span.onclick = function() {
             modal.style.display = "none";
         }
+
+
+
+
+
+        /*** */
+        
