eed98010f87145789dee773c1b2552e1422ea899
roini <r.giorgadze1996@gmail.com>
2023-10-17 22:11:57 +0400

    Update mailsender.js

--- mailsender.js
+++ mailsender.js
@@ -7,13 +7,7 @@
                 var piradinomeri= document.getElementById("piradinomeri").value;
                 var phoneNumber=document.getElementById("phone").value;
                 var replyto = document.getElementById("replyto").value;
-                var typeInt= document.getElementById("typeInt").value;
-                var regions = document.getElementById("regions").value;
-                var CityAndVilages = document.getElementById("CityAndVilages").value;
-                var saidan = document.getElementById("saidan").value;
-                var alreadyus = document.getElementById("alreadyus").value;
-                var why = document.getElementById("why").value;
-                var geocoord = document.ElementById("geocoord").value;
+                
         (
 	function(){emailjs.init("UFa_cH3yFhHBNo1L9"); // Account Public Key
 	   })();
