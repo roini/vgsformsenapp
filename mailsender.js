c0ea86b743737c2b2575f2945ece3473266377e2
roini <r.giorgadze1996@gmail.com>
2023-10-17 22:26:16 +0400

    Update mailsender.js

--- mailsender.js
+++ mailsender.js
@@ -1,49 +1,53 @@
-    function sendMail(){
-        //----------------------------------------------------------------------
-            ///ცვლადების შემოტანა
-                var firstname= document.getElementById("fname").value;
-	        var lastname=document.getElementById("lname").value;
-	        var sendername=firstname + ' ' + lastname ;
-                var piradinomeri= document.getElementById("piradinomeri").value;
-                var phoneNumber=document.getElementById("phone").value;
-                var replyto = document.getElementById("replyto").value;
-	    var typeInt = document.getElementById("typeInt").value;
-	    var regions = document.getElementById("regions").value;
-	    var cityandvilages = document.getElementById("CityAndVilages").value;
-	    var saidan = document.getElementById("saidan").value;
-	    var alreadyus = document.getElementById("alreadyus").value;
-	    var why = document.getElementById("why").value;
-	    var geocoord = document.getElementById("geocoord").value;
-                
-        (
-	function(){emailjs.init("UFa_cH3yFhHBNo1L9"); // Account Public Key
-	   })();
-        var to='r.giorgadze1996@gmail.com'
-	var subject='ონლაინ შეკვეთა!!!';
-	var message='test';
-        var params = {
-        sendername: sendername,
-        to:to,
-        subject:subject,
-        replyto:replyto,
-        piradinomeri:piradinomeri,
-	phonenumber:phoneNumber,
-	typeInt:typeInt,
-	    regions:regions,
-	    cityandvilages:cityandvilages,
-	    saidan:saidan,
-	    alreadyus:alreadyus,
-	    why:why,
-	    geocoord:geocoord
-	
-};
-        console.log(sendername);
-        var serviceID ="service_o9flavu";//Email Service ID
-        var templateID ="template_efrimtd"; // Email Template ID
+function sendMail() {
+    // Variable input extraction
+    var firstname = document.getElementById("fname").value;
+    var lastname = document.getElementById("lname").value;
+    var sendername = firstname + ' ' + lastname;
+    var piradinomeri = document.getElementById("piradinomeri").value;
+    var phoneNumber = document.getElementById("phone").value;
+    var replyto = document.getElementById("replyto").value;
+    var typeInt = document.getElementById("typeInt").value;
+    var regions = document.getElementById("regions").value;
+    var cityAndVillages = document.getElementById("CityAndVilages").value;
+    var saidan = document.getElementById("saidan").value;
+    var alreadyus = document.getElementById("alreadyus").value;
+    var why = document.getElementById("why").value;
+    var geocoord = document.getElementById("geocoord").value;
 
-        emailjs.send(serviceID, templateID, params)
-        .then( res => {
-            alert("Email sent successfully!!")
+    (function() {
+        emailjs.init("UFa_cH3yFhHBNo1L9"); // Account Public Key
+    })();
+
+    var to = 'r.giorgadze1996@gmail.com';
+    var subject = 'ონლაინ შეკვეთა!!!';
+    var message = 'test';
+
+    var params = {
+        sendername: sendername,
+        to: to,
+        subject: subject,
+        replyto: replyto,
+        piradinomeri: piradinomeri,
+        phonenumber: phoneNumber,
+        typeInt: typeInt,
+        regions: regions,
+        cityAndVillages: cityAndVillages,
+        saidan: saidan,
+        alreadyus: alreadyus,
+        why: why,
+        geocoord: geocoord
+    };
+
+    console.log(sendername);
+
+    var serviceID = "service_o9flavu"; // Email Service ID
+    var templateID = "template_efrimtd"; // Email Template ID
+
+    emailjs.send(serviceID, templateID, params)
+        .then(function(res) {
+            alert("Email sent successfully!!");
         })
-        .catch();
-    }
+        .catch(function(error) {
+            console.error('Error sending email:', error);
+        });
+}
